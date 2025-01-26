2458f8b4ee5361f191aeaa136cbc892a00244c25
lenovo <lenovo@LAPTOP-M08NBRRS>
2025-01-26 22:28:59 +0530

    Push

---
+++ src/Task/Increament.java
@@ -0,0 +1,24 @@
+package Task;
+
+public class Increament {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//int a = 10;
+ //(++a + a++ + a++)
+		
+	
+		
+
+		int a=10;
+		System.out.println(++a + a++ + a++);
+
+        System.out.println(a);
+
+	 
+
+	}
+
+}
+  
---
+++ src/Task/Increament1.java
@@ -0,0 +1,15 @@
+package Task;
+
+public class Increament1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int a=20;
+		 System.out.println(--a + a++ + a--);
+		 
+		 System.out.println(a);
+	}
+
+}
